ae53482e43071d49b64d10ce9077ecbaa03dbf4e
EnmanuelJS <galvanenmanuel0@gmail.com>
2025-05-08 22:53:41 -0400

    actividad matrices

--- hello.c
+++ hello.c
@@ -1,85 +1,33 @@
-
 #include <stdio.h>
-#include <conio.h>
+#define TAM 100
 
-// declaracion de variables
-int semestre_ingresado;
-int opcion_input; //se elije la carrera
-int semestre_menor = 2; // se coloco 2 porque se evaluara semestre_ingresado en funcion de si es igual o menor
-int semestre_mayor = 18;
-char usuario[28];
-char opciones[3][50] = {
-    "  1.Ingeniería de sistemas",
-    "  2.Magisterio",
-    "  3.Educación"};
-char materias[3][30] = {
-    "  1.Matemática",
-    "  2.Historia",
-    "  3.Química"};
-
-void seleccion_materias()
-{
-    if (opcion_input <= 2 || opcion_input >= 0)
-    {
-        printf("**Seleccione una materia de la lista**\n");
-
-        switch (opcion_input)
-        {
-        case 1:
-            printf("**Materias a cursar:**\n");
-            printf("%s \n", materias[0]);
-            printf("%s \n", materias[1]);
-            printf("%s \n", materias[2]);
-            break;
-
-        case 2:
-            printf("**Materias a cursar:**\n");
-            printf("%s \n", materias[0]);
-            printf("%s \n", materias[1]);
-            printf("%s \n", materias[2]);
-            break;
-            
-        case 3:
-            printf("**Materias a cursar:**\n");
-            printf("%s \n", materias[0]);
-            printf("%s \n", materias[1]);
-            printf("%s \n", materias[2]);
-            break;
-        }
-    }
-}
-
-// contiene el cuerpo del programa
-void seleccion_semestre()
-{
-    printf("**Seleccione su carrera**\n");
-    printf("%s \n", opciones[0]);
-    printf("%s \n", opciones[1]);
-    printf("%s \n", opciones[2]);
-    scanf("%d", &opcion_input);
-
-    while (opcion_input > 3 || opcion_input <= 0)
-    {
-        printf("La opcion ingresada no es válida\n");
-        scanf("%d", &opcion_input);
-    }
-    seleccion_materias();
-}
-// funcion principal
 int main()
 {
-    printf("Ingrese su usuario\n");
-    scanf("%s", &usuario);
-    printf("Bienvenido: %s \n \n", usuario);
-    printf("Ingrese el semestre que esta cursando\n");
-    scanf("%d", &semestre_ingresado);
+    int m[TAM][TAM],column,row,index_c,index_r;
+    // pidiendo datos al usuario
+    printf("Ingresar numero de columnas\n");
+    scanf("%i",&column);
+    printf("Ingresar numero de filas\n");
+    scanf("%i",&row);
 
-    while (semestre_ingresado > semestre_mayor || semestre_ingresado < 3)
+    // recorriendo filas
+    for(index_r = 0; index_r < row; index_r++)
     {
-        printf("Error! ingrese el semestre nuevamente\n");
-        scanf("%d", &semestre_ingresado);
+        for (index_c = 0; index_c < column; index_c++)
+        {
+            printf("Ingresa las calificaciones en los campos\n");
+            scanf("%i",&m[index_r][index_c]);
+        }
     }
-    seleccion_semestre();
+        
+    printf("Las calificaciones ordenadas en la matriz son:\n");
+    for(index_r = 0; index_r < row; index_c++)
+    {
+        for (index_r = 0; index_r < column; index_r++)
+        {
+            printf("%i",m[index_r][index_c]);
+        }
+        getchar();
+    }
     return 0;
-    getchar();
-}
+};
